86cae1b5bf8c1c71cf3f1dab08ce486eaf42ab79
jhonatan-JS <jhonatanribeiro.mei@hotmail.com>
2020-06-17 22:50:46 -0400

    Aceitar css e img

--- Front-end-com-react/src/App.js
+++ Front-end-com-react/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 
+import backImage from './assets/back.jpeg'
+
+import './App.css';
+
 function App() {
     const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
 
@@ -13,6 +17,8 @@ function App() {
     return (
         <>
         <Header title="Projects" /> 
+
+        <img width={350} src={backImage} alt="background"/>
 
             <ul>
                 {projects.map(project => <li key={project}>{project}</li>)}
